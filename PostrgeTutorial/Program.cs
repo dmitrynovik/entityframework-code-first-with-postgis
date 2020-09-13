f4045f764b7c95e70e1f8f53cba9118c60d8bdce
Dmitry Novik <dmitry.novik@gmail.com>
2020-09-13 20:44:54 +1000

    add some blog data seeding

--- PostrgeTutorial/Program.cs
+++ PostrgeTutorial/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using ConsoleApp.PostgreSQL;
 
@@ -10,8 +11,23 @@ namespace PostrgeTutorial
         {
             try
             {
-                var ctx = new BloggingContext();
+                using var ctx = new BloggingContext();
                 var firstBlog = ctx.Blogs.FirstOrDefault();
+                if (firstBlog == null)
+                {
+                    var seedBlog = new Blog { Name = "Dmitry", Url = "dmitry.livejournal.com" };
+                    ctx.Blogs.Add(seedBlog);
+                    ctx.SaveChanges();
+                    firstBlog = ctx.Blogs.First();
+                }
+
+                if (firstBlog.Posts == null || !firstBlog.Posts.Any())
+                {
+                    var post = new Post {Blog = firstBlog, Content = "lorem ipsum", Title = "My first post"};
+                    firstBlog.Posts = new List<Post> {post};
+                    ctx.SaveChanges();
+                }
+
                 Console.WriteLine($"Found blog: {firstBlog?.Name}");
             }
             catch (Exception e)
